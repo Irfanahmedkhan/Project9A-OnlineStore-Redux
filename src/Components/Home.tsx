eff85b98c2e2e207ce670c83353132773b3b9c31
Irfanahmedkhan <64011793+Irfanahmedkhan@users.noreply.github.com>
2020-09-10 14:55:36 +0300

    Update Home.tsx

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -14,9 +14,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     alignItems: " center",
     height: "85vh",
-
-
-
+    overflow: "auto"
   },
   CardContainer: {
     display: "flex",
@@ -29,7 +27,6 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
     alignItems: " center",
-
   },
 
   Main: {
@@ -50,8 +47,6 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 40,
   },
 
-
-
   media: {
     minHeight: "160px",
     paddingTop: "56.25%", // 16:9
@@ -63,17 +58,15 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     fontSize: "0.5rem",
     minHeight: "80px",
-    width: "100%"
+    width: "100%",
   },
   cardContentText: {
     textAlign: "center",
     maxHeight: "40px",
     fontWeight: 600,
     fontSize: "13px",
-
   },
 }));
-
 
 function Home() {
   const classes = useStyles();
@@ -86,41 +79,55 @@ function Home() {
         }} > NIKE STORE </Typography>
 
       </Grid> */}
-      <Grid item >
-        <Grid item style={{height: "5vh"}}>
-          <Typography component="p" style={{
-            color: "white", textAlign: "left", fontWeight: 600, letterSpacing: "2px", fontFamily: "Barlow Condensed", marginTop: "20px", marginLeft: "20px", fontSize: "25px"}} > 
-            New Arrival </Typography>
+      <Grid item>
+        <Grid item style={{ height: "5vh" }}>
+          <Typography
+            component="p"
+            style={{
+              color: "white",
+              textAlign: "left",
+              fontWeight: 600,
+              letterSpacing: "2px",
+              fontFamily: "Barlow Condensed",
+              marginTop: "20px",
+              marginLeft: "20px",
+              fontSize: "25px",
+            }}
+          >
+            New Arrival{" "}
+          </Typography>
         </Grid>
-        <Grid item  className={classes.Main}>
-
+        <Grid item className={classes.Main}>
           {NewArival.map((New, i) => {
             console.log(New);
 
             return (
-
-              <Grid item xs={12} sm={4} md={3} key={i} className={classes.CardContainer} >
-                <Link to="/Products" >
-
-                <Card className={classes.Card}>
-                  <CardMedia className={classes.media} image={New?.images} />
-                  <CardContent className={classes.cardContent}>
-                    <Typography component="p" className={classes.cardContentText} >
-                      {New?.name} </Typography>
-                  </CardContent>
-                </Card>
-          </Link>
-
+              <Grid
+                item
+                xs={12}
+                sm={4}
+                md={3}
+                key={i}
+                className={classes.CardContainer}
+              >
+                <Link to="/Products">
+                  <Card className={classes.Card}>
+                    <CardMedia className={classes.media} image={New?.images} />
+                    <CardContent className={classes.cardContent}>
+                      <Typography
+                        component="p"
+                        className={classes.cardContentText}
+                      >
+                        {New?.name}{" "}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Link>
               </Grid>
-
             );
           })}
-
         </Grid>
       </Grid>
-
-       
-
     </Grid>
   );
 }
